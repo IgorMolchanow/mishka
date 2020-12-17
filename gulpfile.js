c8b17c0d0caa2bb4f3a8dacd50dfd0ab62819312
IgorM <igoratmosphere57@gmail.com>
2020-12-17 15:06:33 +0300

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+//  CSS
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const sourcemaps = require("gulp-sourcemaps");
+const watch = require("gulp-watch");
+
+gulp.task("sass-compile", function () {
+  return gulp.src('./source/sass/**/*.scss')
+  .pipe(sourcemaps.init())
+  .pipe(sass().on("error", sass.logError))
+  .pipe(sourcemaps.write('./'))
+  .pipe(gulp.dest('./css/'))
+})
