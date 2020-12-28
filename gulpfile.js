a15baec156cd4e3fcea38b8211c90fd214a405ef
IgorM <igoratmosphere57@gmail.com>
2020-12-28 12:02:25 +0300

    вроде работает

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,37 @@
-//  CSS
-const gulp = require("gulp");
-const plumber = require("gulp-plumber");
-const sourcemap = require("gulp-sourcemaps");
-const sass = require("gulp-sass");
-const postcss = require("gulp-postcss");
-const autoprefixer = require("autoprefixer");
-const server = require("browser-sync").create();
-const rename = require("gulp-rename");
+"use strict";
+
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var sourcemap = require("gulp-sourcemaps");
+var sass = require("gulp-sass");
+var postcss = require("gulp-postcss");
+var autoprefixer = require("autoprefixer");
+var server = require("browser-sync").create();
 
 gulp.task("css", function () {
-  return gulp.src('./source/sass/style.scss')
+  return gulp.src("source/sass/style.scss")
     .pipe(plumber())
     .pipe(sourcemap.init())
     .pipe(sass())
-    .pipe(postcss([autoprefixer()]))
-    .pipe(rename('style.css'))
+    .pipe(postcss([
+      autoprefixer()
+    ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest('build/css'))
+    .pipe(gulp.dest("source/css"))
     .pipe(server.stream());
 });
+
 gulp.task("server", function () {
   server.init({
-    server: "build/",
+    server: "source/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
+
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/js/**/*.js", gulp.series("js", "refresh"));
-  gulp.watch("source/img/**/*.svg", gulp.series("copy", "images", "sprite", "refresh"));
-  gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  gulp.watch("source/*.html").on("change", server.reload);
 });
-//  Перезапуск сервера
-gulp.task("refresh", function (done) {
-  server.reload();
-  done();
-});
-//  Копирование файлов
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/fonts/**/*.{woff,woff2}",
-    "source/img/**/*.{png,jpg,svg}",
-    "source/*.ico"
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
-});
-gulp.task("build", gulp.series(
-  "copy",
-  "css",
-));
-gulp.task("start", gulp.series("build", "server"));
+
+gulp.task("start", gulp.series("css", "server"));
